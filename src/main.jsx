65ebaf7d8b7bd1386f8d9e8740318b4733152403
shahmerrkhan <m.shahmeer.khan8@gmail.com>
2026-04-28 17:45:11 -0400

    Fix Vercel analytics for Vite/React

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { inject } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
+import { createRoot } from 'react-dom/client'
 
-inject()
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
